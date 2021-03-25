6e65d48701f1966ea74c5459c62b8b9426a51577
She <pter.zh@gmail.com>
2021-03-25 20:25:40 +0800

    import directly from rclone fs

--- fs/log_test.go
+++
@@ -1,6 +0,0 @@
-package fs
-
-import "github.com/spf13/pflag"
-
-// Check it satisfies the interface
-var _ pflag.Value = (*LogLevel)(nil)
--- fs/versioncheck.go
+++
@@ -1,7 +0,0 @@
-//+build !go1.10
-
-package fs
-
-// Upgrade to Go version 1.10 to compile rclone - latest stable go
-// compiler recommended.
-func init() { Go_version_1_10_required_for_compilation() }
